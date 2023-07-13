2594f41cb9c48a682b5fc57819eac4ad90ef40bb
GAURAV GOYAL <116831689+gauravgoyal467@users.noreply.github.com>
2023-07-13 11:56:56 +0000

    new sol

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 
 import React from "react";
-import './App.css';
+import './../styles/App.css';
 import Parent from "./Parent";
 
 const App = () => {
   return (
     <div>
-        <Parent/>
+       <Parent/>
     </div>
   )
 }
